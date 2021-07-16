50e3d7bb682551367ec95e89efc20d3f314518f5
Tuyet Nguyen <tuyetbqcba55@gmail.com>
2021-07-15 23:13:08 -0500

    updated index.js

--- models/index.js
+++ models/index.js
@@ -6,11 +6,34 @@ const ProductTag = require('./ProductTag');
 
 // Products belongsTo Category
 
+Product.belongsTo(Category, {
+  foregnKey: 'category_id',
+});
+
 // Categories have many Products
+Category.hasMany(Product, {
+  foreignkey: 'category_id',
+  onDelete: 'CASCADE',
+});
 
 // Products belongToMany Tags (through ProductTag)
 
+Product.belongsToMany(Tag, {
+  through: {
+    model: ProductTag,
+    unique: false,
+    foreignKey: "product_tags"
+  }
+});
+
 // Tags belongToMany Products (through ProductTag)
+Tag.belongsToMany(Product, {
+  through: {
+    model: ProductTag,
+    unique: false,
+    foreignKey: "product_tags"
+  }
+});
 
 module.exports = {
   Product,
